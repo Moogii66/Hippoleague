a6dacb39bcea73271a16e9f2d8476557493f2de3
temka8899 <temuleon8899@gmail.com>
2021-05-13 09:57:20 +0800

    temka

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,6 +1,19 @@
 import React, {useState} from 'react';
-import {Text, StyleSheet, SafeAreaView, Pressable, Button} from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Pressable,
+  Button,
+  StatusBar,
+  View,
+  Dimensions,
+} from 'react-native';
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+console.log(['Width', windowWidth]);
+console.log(['Height', windowHeight]);
 const DashboardScreen = ({navigation}) => {
   const [titleText, setTitleText] = useState("Bird's Nest");
   const [bodyText, setBodyText] = useState('This is not really a bird nest.');
@@ -9,8 +22,18 @@ const DashboardScreen = ({navigation}) => {
   };
 
   return (
-    <SafeAreaView>
-      <Button title="Back" onPress={() => navigation.pop()}></Button>
+    <View>
+      <StatusBar
+        translucent
+        backgroundColor="#5E8D48"
+        barStyle="light-content"
+      />
+      <View style={styles.AppBar}>
+        <Button title="Back" onPress={() => navigation.pop()}></Button>
+        <Text style={{color: 'white', paddingLeft: windowWidth / 2}}>
+          qweqweqwe
+        </Text>
+      </View>
       <Text style={styles.baseText}>
         <Pressable onPress={onPressTitle}>
           <Text style={styles.titleText}>
@@ -21,7 +44,7 @@ const DashboardScreen = ({navigation}) => {
         </Pressable>
         <Text numberOfLines={5}>{bodyText}</Text>
       </Text>
-    </SafeAreaView>
+    </View>
   );
 };
 
@@ -32,6 +55,12 @@ const styles = StyleSheet.create({
   titleText: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  AppBar: {
+    height: 100,
+    backgroundColor: 'grey',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
   },
 });
 
