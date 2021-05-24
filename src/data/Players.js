3d650def647bda67ba03aec2fa4f0f5c98af889b
Moogii66 <moogii67890@gmail.com>
2021-05-24 16:09:03 +0800

    moogii 5 24

--- src/data/Players.js
+++ src/data/Players.js
@@ -3,4 +3,24 @@ export const userData = [
     name: 'Moogii',
     score: '220',
   },
+  {
+    name: 'Moogii',
+    score: '220',
+  },
+  {
+    name: 'Moogii',
+    score: '220',
+  },
+  {
+    name: 'Moogii',
+    score: '220',
+  },
+  {
+    name: 'Moogii',
+    score: '220',
+  },
+  {
+    name: 'Moogii',
+    score: '220',
+  },
 ];
